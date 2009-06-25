209ecb6b380c2a97acb9e649050f9f2cdf08e959
Ryan Masuga <ryan@masugadesign.com>
2009-06-25 11:20:37 -0400

    Added optional mailto="off" parameter. Minor cleanup.

--- plugins/pi.md_spam_me_not.php
+++ plugins/pi.md_spam_me_not.php
@@ -7,19 +7,19 @@ Purpose: Encode email addresses to make it harder for spammers to harvest them.
 */
 
 $plugin_info = array(
-						'pi_name'			=> 'MD Spam Me Not',
-						'pi_version'			=> '1.0.4',
-						'pi_author'			=> 'Ryan Masuga',
-						'pi_author_url'		=> 'http://www.masugadesign.com/',
-						'pi_description'		=> 'Obfuscates email in a way still works when Javascript is disabled.',
-						'pi_usage'			=> Md_spam_me_not::usage()
-					);
+			'pi_name'	       => 'MD Spam Me Not',
+			'pi_version'     => '1.0.5',
+			'pi_author'      => 'Ryan Masuga',
+			'pi_author_url'  => 'http://www.masugadesign.com/',
+			'pi_description' => 'Obfuscates email in a way that still works when Javascript is disabled.',
+			'pi_usage'			 => Md_spam_me_not::usage()
+		);
 
 class Md_spam_me_not {
 
-var $return_data = "";
-var $hiddenemail = "";
-var $show = "";
+  var $return_data = "";
+  var $hiddenemail = "";
+  var $show = "";
 	
 	function md_spam_me_not()
 	{
@@ -31,6 +31,12 @@ var $show = "";
 			$title = $TMPL->fetch_param('title'); //OPTIONAL
 			$subject = $TMPL->fetch_param('subject'); //OPTIONAL
 			$class = $TMPL->fetch_param('class'); //OPTIONAL
+			$mailto = $TMPL->fetch_param('mailto'); //OPTIONAL
+			
+			if(!$email)
+		  {
+			  return;
+			}
 			
 			if ($mode == ""){$mode = "1";}
 			
@@ -50,19 +56,30 @@ var $show = "";
 					//return "ERROR: wrong encoding mode.";
 			}
 		}
-		$show = ($text=="") ? $encodedString : $text;
 		
-		$myclass = '';
-		if($class != '') $myclass = " class=\"" . $class . "\"";
 		
-		$subject_line = '';
-		if($subject != '') $subject_line = "?subject=$subject";
+		if($mailto != '') 
+		{
+		  $hiddenemail = $encodedString;
+		}
+		else
+		{
+  		$show = ($text=="") ? $encodedString : $text;
+		
+  		$myclass = '';
+  		if($class != '') $myclass = " class=\"" . $class . "\"";
+		
+  		$subject_line = '';
+  		if($subject != '') $subject_line = "?subject=$subject";
 				
-		$title_attr = '';
-		if($title != '') $title_attr = " title=\"" . $title . "\"";
+  		$title_attr = '';
+  		if($title != '') $title_attr = " title=\"" . $title . "\"";
 		
-		$hiddenemail = "<a href=\"mailto:$encodedString$subject_line\" $myclass $title_attr>$show</a>";
+  		$hiddenemail = "<a href=\"mailto:$encodedString$subject_line\" $myclass $title_attr>$show</a>";
+
+		}
 		$this->return_data = $hiddenemail;
+		
 	}
     
 // ----------------------------------------
@@ -78,7 +95,7 @@ Place the following tag in any of your templates:
 {exp:md_spam_me_not}
 
 PARAMETERS: 
-The tag has six parameters:
+The tag has seven parameters:
 
 1. email - The email address to obfuscate. [REQUIRED]
 2. mode - 1 or 2. 1 = decimal mode and 2 = Hexadecimal [OPTIONAL, defaults to 1]
@@ -86,9 +103,15 @@ The tag has six parameters:
 4. title - [OPTIONAL]
 5. class - [OPTIONAL]
 6. subject - [OPTIONAL]
+7. mailto - [OPTIONAL] Set this to "off" and every other parameter will be ignored - it will return only the
+            email address.
 
 Example usage (full):  {exp:md_spam_me_not email="ryan@masugadesign.com" mode="1" title="My Title" class="nocion email" subject="EE Inquiry" text="Email Ryan for more info."}
+
 Example usage (basic):  {exp:md_spam_me_not email="ryan@masugadesign.com"}
+
+Example usage (mailto):  {exp:md_spam_me_not email="ryan@masugadesign.com" mailto="off"}
+
 
 <?php
 $buffer = ob_get_contents();
